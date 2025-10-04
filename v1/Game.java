bd71f7e98d6d777a4eb2ed8962d7a1e0a0859618
Tenseikage <christaratibu51@gmail.com>
2025-10-04 15:41:11 +0200

    first commit

---
+++ v1/Game.java
@@ -0,0 +1,124 @@
+package v1;
+/**
+ * Classe Game - le moteur du jeu d'aventure Zuul.
+ *
+ * @author TARATIBU Christophe
+ */
+public class Game
+{ 
+    private Room aCurrentRoom;
+    private Parser aParser;
+
+    
+
+    private void createRooms(){
+        Room vOutside = new Room("outside the main entrance of the university");
+        Room vTheatre = new Room("in a lecture theatre");
+        Room vPub = new Room("in the campus pub");
+        Room vLab = new Room("in a computing lab");
+        Room vOffice = new Room("in the computing admin office");
+
+        vOutside.setExits(null,vTheatre,vLab,vPub);
+        vTheatre.setExits(null,null,null, vOutside);
+        vPub.setExits(null,vOutside,null,null);
+        vLab.setExits(vOutside,vOffice,null,null);
+        vOffice.setExits(null,null,null,vLab);
+        
+        this.aCurrentRoom = vOutside;
+    }
+    private void goRooms(Command pCmd){
+        if(!pCmd.hasSecondWord()){
+            System.out.println("Go where ?");
+        }
+        Room vNextRoom = null;
+        String vDirection = pCmd.getSecondWord();
+        System.out.println(vDirection);
+        switch (vDirection) {
+            case "north":
+                vNextRoom = this.aCurrentRoom.aNorthExit;
+                break;
+        
+            case "south":
+                vNextRoom = this.aCurrentRoom.aSouthExit;
+                break;
+
+            case "east":
+                vNextRoom = this.aCurrentRoom.aEastExit;
+                break;
+            case "west":
+                vNextRoom = this.aCurrentRoom.aWestExit;
+                break;
+            default:
+                System.out.println("Unknown direction");
+                break;
+        }
+        if(vNextRoom == null){
+            System.out.println("There's no door");
+        } else {
+            this.aCurrentRoom = vNextRoom;
+            System.out.println(this.aCurrentRoom.getDescription());
+            System.out.println("Possible exits");
+            System.out.println(this.aCurrentRoom.aEastExit  != null ? "-east" : "");
+            System.out.println(this.aCurrentRoom.aWestExit  != null ? "-west" : "");
+            System.out.println(this.aCurrentRoom.aNorthExit  != null ? "-north" : "");
+            System.out.println(this.aCurrentRoom.aSouthExit  != null ? "-nouth" : "");
+        }
+    }
+
+    public void welcome(){
+        System.out.println("Welcome to the World of Zuul! \nWorld of Zuul is a new, incredibly boring adventure game." +
+        "\nType 'help' if you need help. \nYou are outside the main entrance of the university \nExits: east south west");
+    }
+
+    public void printHelp(){
+        System.out.println("You are lost. You are alone.\r\n" + //
+                        "You wander around at the university.\r\n" + //
+                        "\r\n" + //
+                        "Your command words are:\r\n" + //
+                        "  go quit help");
+    }
+
+    private boolean quit(final Command pCmd){
+        if (pCmd.hasSecondWord()) {
+            System.out.println("Quit what ?");
+            return false;
+        }
+        return true;
+    }
+    
+    private boolean processCommand(final Command pCmd) {
+        if (pCmd.isUnknown()) {
+            System.out.println("I don't know what you mean...");
+            return false;
+        }
+
+        String vCommandWord = pCmd.getCommandWord();
+        if ("help".equals(vCommandWord )) {
+            printHelp();
+            return false;
+        } else if ("go".equals(vCommandWord )) {
+            goRooms(pCmd);
+            return false;
+        } else if ("quit".equals(vCommandWord)) {
+            return quit(pCmd);
+        } else {
+            System.out.println("Erreur du programmeur : commande non reconnue !");
+            return true;
+        }
+    }
+
+    public void play() {
+        this.welcome();
+        boolean vFinished = false;
+        while (!vFinished) {
+            Command vCommand = this.aParser.getCommand();
+            vFinished = this.processCommand(vCommand);
+        }
+        System.out.println("Thank you for playing.  Good bye.");
+    }
+    public Game(){
+        this.aParser = new Parser();
+        this.createRooms();
+        play();
+    }
+} // Game
---
+++ v1/Room.java
@@ -0,0 +1,28 @@
+package v1;
+/**
+ * Classe Room - un lieu du jeu d'aventure Zuul.
+ *
+ * @author Christophe TARATIBU
+ */
+public class Room
+{
+    private String aDescription;
+    public Room aNorthExit;
+    public Room aEastExit;
+    public Room aSouthExit;
+    public Room aWestExit;
+    public Room(final String pDescription){
+        this.aDescription = pDescription;  
+    }
+    
+    public String getDescription(){
+        return this.aDescription;
+    }
+
+    public void setExits(final Room pNorthExit,final Room pEastExit, final Room pSouthExit, final Room pWestExit){
+        this.aNorthExit = pNorthExit;
+        this.aEastExit = pEastExit;
+        this.aSouthExit = pSouthExit;
+        this.aWestExit = pWestExit;
+    }
+} // Room
